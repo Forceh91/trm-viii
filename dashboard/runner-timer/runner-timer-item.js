378c54ee867fefdbce8f6eaf66992d6b237620c2
Matt Hadden <forceh2010@gmail.com>
2021-07-14 18:56:21 -0400

    runner-timer-item: Disable make audible button Techland: Update styling

--- dashboard/runner-timer/runner-timer-item.js
+++ dashboard/runner-timer/runner-timer-item.js
@@ -4,173 +4,189 @@ import { PolymerElement } from "../../node_modules/@polymer/polymer/polymer-elem
 const obsAudioSources = nodecg.Replicant("obsAudioSources");
 
 class DashboardRunnerTimerItem extends PolymerElement {
-	static get template() {
-		return html`
-			<link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+  static get template() {
+    return html`
+      <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
 
-			<style>
-				* {
-					box-sizing: border-box;
-				}
+      <style>
+        * {
+          box-sizing: border-box;
+        }
 
-				#body {
-					align-items: center;
-					display: flex;
-					width: 100%;
-				}
+        #body {
+          align-items: center;
+          display: flex;
+          width: 100%;
+        }
 
-				.right-side {
-					margin-left: auto;
-				}
+        .right-side {
+          margin-left: auto;
+        }
 
-				.time {
-					margin-right: 10px;
-				}
+        .time {
+          margin-right: 10px;
+        }
 
-				.right-align {
-					text-align: right;
-				}
+        .right-align {
+          text-align: right;
+        }
 
-				button {
-					float: right;
-					margin: 0;
-					width: 100px;
-				}
+        button {
+          float: right;
+          margin: 0;
+          width: 100px;
+        }
 
-				button:not(:last-child) {
-					margin-right: 10px;
-				}
+        button:not(:last-child) {
+          margin-right: 10px;
+        }
 
-				button.danger:not([disabled]) {
-					background: indianred;
-					color: #fff;
-				}
+        button.danger:not([disabled]) {
+          background: indianred;
+          color: #fff;
+        }
 
-				.start {
-					background: green;
-				}
+        .start {
+          background: green;
+        }
 
-				.pause {
-					background: purple;
-				}
+        .pause {
+          background: purple;
+        }
 
-				.finish {
-					background: darkorange;
-				}
+        .finish {
+          background: darkorange;
+        }
 
-				.reset {
-					background: darkred;
-				}
+        .reset {
+          background: darkred;
+        }
 
-				.hidden {
-					display: none;
-				}
+        .hidden {
+          display: none;
+        }
 
-				.pos {
-					background: purple;
-					font-weight: bold;
-					margin-right: 10px;
-					padding: 5px;
-					text-align: center;
-				}
-			</style>
+        .pos {
+          background: purple;
+          font-weight: bold;
+          margin-right: 10px;
+          padding: 5px;
+          text-align: center;
+        }
+      </style>
 
-			<div id="body">
-				<div class$="[[getPositionClass()]]">[[timer.position]]</div>
-				<div>[[timer.runner]]</div>
-				<div class="right-side time">
-					<span class$="[[shouldShowTime()]]">[[timer.formattedTime]]</span>
-				</div>
+      <div id="body">
+        <div class$="[[getPositionClass()]]">[[timer.position]]</div>
+        <div>[[timer.runner]]</div>
+        <div class="right-side time">
+          <span class$="[[shouldShowTime()]]">[[timer.formattedTime]]</span>
+        </div>
 
-				<button type="button" class="btn btn-sm btn-success" id="startTimer" class="start" disabled$="[[shouldDisableIfNotFinished()]]">
-					Resume
-				</button>
+        <button
+          type="button"
+          class="btn btn-sm btn-success"
+          id="startTimer"
+          class="start"
+          disabled$="[[shouldDisableIfNotFinished()]]"
+        >
+          Resume
+        </button>
 
-				<button type="button" class="btn btn-sm btn-primary" id="finishTimer" class="finish" disabled$="[[shouldDisableWhenNotRunning()]]">
-					Finish
-				</button>
+        <button
+          type="button"
+          class="btn btn-sm btn-primary"
+          id="finishTimer"
+          class="finish"
+          disabled$="[[shouldDisableWhenNotRunning()]]"
+        >
+          Finish
+        </button>
 
-				<button type="button" class="btn btn-sm btn-danger" id="resetTimer" class="reset" disabled$="[[shouldDisableWhenRunning()]]">Reset</button>
+        <button
+          type="button"
+          class="btn btn-sm btn-danger"
+          id="resetTimer"
+          class="reset"
+          disabled$="[[shouldDisableWhenRunning()]]"
+        >
+          Reset
+        </button>
+      </div>
+    `;
+  }
 
-				<button type="button" class="btn btn-sm btn-warning" id="toggleSound" class="audio" disabled$="[[shouldDisableSoundOption()]]">
-					Make Audible
-				</button>
-			</div>
-		`;
-	}
+  static get is() {
+    return "dashboard-runner-timer-item";
+  }
 
-	static get is() {
-		return "dashboard-runner-timer-item";
-	}
+  static get properties() {
+    return {
+      ix: Number,
+      timer: Object,
+    };
+  }
 
-	static get properties() {
-		return {
-			ix: Number,
-			timer: Object,
-		};
-	}
+  ready() {
+    super.ready();
 
-	ready() {
-		super.ready();
+    this.$.startTimer.addEventListener("click", this._startTimer.bind(this));
+    this.$.finishTimer.addEventListener("click", this._finishTimer.bind(this));
+    this.$.resetTimer.addEventListener("click", this._resetTimer.bind(this));
+    // this.$.toggleSound.addEventListener("click", this._toggleSound.bind(this));
+  }
 
-		this.$.startTimer.addEventListener("click", this._startTimer.bind(this));
-		this.$.finishTimer.addEventListener("click", this._finishTimer.bind(this));
-		this.$.resetTimer.addEventListener("click", this._resetTimer.bind(this));
-		this.$.toggleSound.addEventListener("click", this._toggleSound.bind(this));
-	}
+  shouldDisableWhenRunning() {
+    return this.timer.state === 1;
+  }
 
-	shouldDisableWhenRunning() {
-		return this.timer.state === 1;
-	}
+  shouldDisableWhenNotRunning() {
+    return this.timer.state !== 1;
+  }
 
-	shouldDisableWhenNotRunning() {
-		return this.timer.state !== 1;
-	}
+  shouldDisableIfNotFinished() {
+    return this.timer.state !== 3;
+  }
 
-	shouldDisableIfNotFinished() {
-		return this.timer.state !== 3;
-	}
+  isFinished() {
+    return this.timer.state === 3;
+  }
 
-	isFinished() {
-		return this.timer.state === 3;
-	}
+  audioSource() {
+    return this.ix >= 0 && obsAudioSources && this.ix < obsAudioSources.value.length && obsAudioSources.value[this.ix];
+  }
 
-	audioSource() {
-		return this.ix >= 0 && obsAudioSources && this.ix < obsAudioSources.value.length && obsAudioSources.value[this.ix];
-	}
+  shouldDisableSoundOption() {
+    const audioSource = this.audioSource();
+    return obsAudioSources.value.length < 2 || !audioSource || !audioSource.muted;
+  }
 
-	shouldDisableSoundOption() {
-		const audioSource = this.audioSource();
-		return obsAudioSources.value.length < 2 || !audioSource || !audioSource.muted;
-	}
+  getPositionClass() {
+    return this.isFinished() ? "pos" : "hidden";
+  }
 
-	getPositionClass() {
-		return this.isFinished() ? "pos" : "hidden";
-	}
+  shouldShowTime() {
+    return this.isFinished() ? "time" : "hidden";
+  }
 
-	shouldShowTime() {
-		return this.isFinished() ? "time" : "hidden";
-	}
+  _startTimer() {
+    nodecg.sendMessage("runnertimers:start", this.timer.id);
+  }
 
-	_startTimer() {
-		nodecg.sendMessage("runnertimers:start", this.timer.id);
-	}
+  _pauseTimer() {
+    nodecg.sendMessage("runnertimers:pause", this.timer.id);
+  }
 
-	_pauseTimer() {
-		nodecg.sendMessage("runnertimers:pause", this.timer.id);
-	}
+  _finishTimer() {
+    nodecg.sendMessage("runnertimers:finish", this.timer.id);
+  }
 
-	_finishTimer() {
-		nodecg.sendMessage("runnertimers:finish", this.timer.id);
-	}
+  _resetTimer() {
+    nodecg.sendMessage("runnertimers:reset", this.timer.id);
+  }
 
-	_resetTimer() {
-		nodecg.sendMessage("runnertimers:reset", this.timer.id);
-	}
-
-	_toggleSound() {
-		nodecg.sendMessage("runner:togglesound", { runnerIx: this.ix, muteStatus: !this.audioSource().muted });
-	}
+  _toggleSound() {
+    nodecg.sendMessage("runner:togglesound", { runnerIx: this.ix, muteStatus: !this.audioSource().muted });
+  }
 }
 
 customElements.define(DashboardRunnerTimerItem.is, DashboardRunnerTimerItem);
